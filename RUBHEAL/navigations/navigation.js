89872e2bfba9ade8adf2c221340e1d1a4d0b99ff
Chanakarn <64070018@it.kmitl.ac.th>
2023-09-17 19:50:53 +0700

    bottom tab nav

--- RUBHEAL/navigations/navigation.js
+++ RUBHEAL/navigations/navigation.js
@@ -1,8 +1,15 @@
 import React from "react";
+import { AntDesign } from "@expo/vector-icons";
+
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+
+
 import CartScreen from "../screens/cartScreen.js"
 import HomeScreen from "../screens/HomeScreen.js"
+import RankScreen from "../screens/RankScreen.js"
 import chatScreen from "../screens/chatScreen.js";
 import messageScreen from "../screens/messageScreen.js";
 import profile from "../screens/profile.js";
@@ -12,7 +19,36 @@ import pageProductForAdmin from "../screens/pageProductForAdmin.js";
 import customerBuyProduct from "../screens/customerBuyProduct.js";
 
 
+
+
 const CartNavigator = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+function TabNavigator(){
+    
+    return (
+        <Tab.Navigator initialRouteName="HomeScreen">
+            <Tab.Screen name="HOME" component={HomeScreen} options={{ headerShown: false,
+                tabBarIcon: ({ color, size }) => {
+                    return <AntDesign name="home" size={26} color={color} />;
+                }, }} />
+            <Tab.Screen name="TOP 10" component={RankScreen} options={{ headerShown: false,
+                tabBarIcon: ({ color, size }) => {
+                    return <AntDesign name="faHotjar" size={26} color={color} />;
+                }, }} />
+            <Tab.Screen name="Chat" component={messageScreen} options={{ headerShown: false,
+                tabBarIcon: ({ color, size }) => {
+                    return <AntDesign name="wechat" size={26} color={color} />;
+                }, }} />
+            <Tab.Screen name="Me" component={profile} options={{ headerShown: false,
+                tabBarIcon: ({ color, size }) => {
+                    return <AntDesign name="user" size={26} color={color} />;
+                },}} />
+        </Tab.Navigator>
+    )
+
+}
+
+
 function Navigation() {
     return (
         <CartNavigator.Navigator initialRouteName="Cart">
@@ -27,7 +63,7 @@ function Navigation() {
 export default function Mainavigation() {
     return (
         <NavigationContainer>
-            <Navigation />
+            <TabNavigator />
         </NavigationContainer>
     );
 }
