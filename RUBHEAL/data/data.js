6e079d26a779af771ccbe5bac8eeddf3129a5822
PUTTIPONG3035 <88504764+PUTTIPONG3035@users.noreply.github.com>
2023-09-18 20:31:22 +0700

    Update data.js

--- RUBHEAL/data/data.js
+++ RUBHEAL/data/data.js
@@ -1,12 +1,12 @@
 import Chat from "../model/chat";
 
 export const CHAT   = [
-    new Chat("1", "Folk", "https://img.freepik.com/free-photo/puppy-that-is-walking-snow_1340-37228.jpg", "My name is Folk", "20", "10"),
-    new Chat("2", "Folk", "https://img.freepik.com/free-photo/puppy-that-is-walking-snow_1340-37228.jpg", "My name is Folk", "20", "10"),
-    new Chat("3", "Folk", "https://img.freepik.com/free-photo/puppy-that-is-walking-snow_1340-37228.jpg", "My name is Folk", "20", "10"),
-    new Chat("4", "Folk", "https://img.freepik.com/free-photo/puppy-that-is-walking-snow_1340-37228.jpg", "My name is Folk", "20", "10"),
-    new Chat("5", "Folk", "https://img.freepik.com/free-photo/puppy-that-is-walking-snow_1340-37228.jpg", "My name is Folk", "20", "10"),
-    new Chat("6", "Folk", "https://img.freepik.com/free-photo/puppy-that-is-walking-snow_1340-37228.jpg", "My name is Folk", "20", "10"),
-    new Chat("7", "Folk", "https://img.freepik.com/free-photo/puppy-that-is-walking-snow_1340-37228.jpg", "My name is Folk", "20", "10"),
-    new Chat("8", "Folk", "https://img.freepik.com/free-photo/puppy-that-is-walking-snow_1340-37228.jpg", "My name is Folk", "20", "10"),
+    new Chat("1", "Folk", "https://hips.hearstapps.com/hmg-prod/images/beautiful-smooth-haired-red-cat-lies-on-the-sofa-royalty-free-image-1678488026.jpg?crop=1xw:0.84415xh;center,top", "My name is Folk", "20", "10"),
+    new Chat("2", "BlackDog", "https://picsum.photos/id/237/200/300", "My name is BlackDog", "15", "30"),
+    new Chat("3", "Mountains", "https://picsum.photos/seed/picsum/200/300", "My name is Mountains", "5", "15"),
+    new Chat("4", "Gray", "https://picsum.photos/200/300?grayscale", "My name is Gray", "20", "10"),
+    new Chat("5", "Cat", "https://i.natgeofe.com/n/548467d8-c5f1-4551-9f58-6817a8d2c45e/NationalGeographic_2572187_square.jpg", "My name is Cat", "25", "20"),
+    new Chat("6", "Popeye", "https://static.wikia.nocookie.net/deathbattle/images/3/3f/Portrait.popeye.png/revision/latest/thumbnail/width/360/height/450?cb=20211216064418", "My name is Popeye", "20", "10"),
+    new Chat("7", "Shark", "https://t3.ftcdn.net/jpg/00/80/09/38/360_F_80093845_aYB5ErmGYK7g9EUY0HQoPqeb9nnAwFjN.jpg", "My name is Shark", "15", "10"),
+    new Chat("8", "Toy", "https://storage.googleapis.com/techsauce-prod/ugc/uploads/2022/7/1658826428_Toy_Story.jpg", "My name is Toy", "20", "10"),
 ];
